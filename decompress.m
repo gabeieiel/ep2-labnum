d524c3a8cf8c039db75a6dc7a93f04cce5a1c289
GuilhermeKGulka <kgulkaguilherme@usp.br>
2025-05-09 22:50:52 -0300

    correcao do codigo

--- decompress.m
+++ decompress.m
@@ -5,18 +5,17 @@ function [] = decompress(compressed_image, method, k, h)
     n = cmp_a; % como imagem e quadrada, cmp_a = cmp_l
     p = n + (n-1)*k; % (k+1)(n-1)+1
     decmp_img = zeros(p, p, c, 'uint8');
+    hIk = h/k;
 
     B = zeros(4, 4, 'single');
 
     if (method == 1)
-        B(:, 1) = 1;
-        B(2, 3) = h;
-        B(3, 2) = h;
-        B(4, 2) = h;
-        B(4, 3) = h;
-        B(4, 4) = h*h;
+        B = [1 0 0 0;
+             1 0 h 0;
+             1 h 0 0;
+             1 h h h^2];
 
-        M = zeros(4, 1, c); %Matriz(vetor coluna) que guarda os valores avaliados para calculo de A(vetor colunas dos coef.)
+        M = zeros(4, 1); %Matriz(vetor coluna) que guarda os valores avaliados para calculo de A(vetor colunas dos coef.)
     endif
 
     if (method == 2) 
@@ -53,26 +52,19 @@ function [] = decompress(compressed_image, method, k, h)
             dxdy(n, t, :) = (dy(n, t, :) - dy(n-1, t, :))/h;
         endfor
 
-        B(1,1) = 1;
-        B(2,1) = 1;
-        B(2,2) = h;
-        B(2,3) = h*h;
-        B(2,4) = h*h*h;
-        B(3,2) = 1;
-        B(4,2) = 1;
-        B(4,3) = 2*h;
-        B(4,4) = 3*h*h;
+        B = [1 0 0 0;
+             1 h h^2 h^3;
+             0 1 0 0;
+             0 1 2*h 3*h^2];
 
         M = zeros(4, 4); %Matriz que guarda os valores avaliados para calculo de A(matriz dos coef.)
     endif
 
     B_inv = inv(B);
 
-    % Calculo dos coeficientes polinomiais e interpolacao
-    % (I, J): indices em decmp_img || (i, j): indices em cmp_img
     for i = 0:n-2 
         for j = 0:n-2
-            % Iterando sobre os (xi, yi), cantos inferiores esquerdos dos quadrados 
+            % Iterando sobre os (i, j), cantos inferiores esquerdos dos quadrados 
             if (method == 1)
                 for ch = 1:c % Para cada canal
                     M(1, 1) = cmp_img(i+1, j+1, ch);
@@ -80,11 +72,11 @@ function [] = decompress(compressed_image, method, k, h)
                     M(3, 1) = cmp_img(i+2, j+1, ch);
                     M(4, 1) = cmp_img(i+2, j+2, ch);
 
-                    A = B_inv * M;
+                    A = B \ M; %Resolve o sistema B*A = M
                     % Iterando sobre os pixels do quadrado, inclui todas as arestas
-                    for di = 0:k+1
-                        for dj = 0:k+1
-                            decmp_img(1+(k+1)*i+di, 1+(k+1)*j+dj, ch) = A(1,1) + A(2,1)*di + A(3,1)*dj + A(4,1)*di*dj;
+                    for I = 0:k+1
+                        for J = 0:k+1
+                            decmp_img(1+(k+1)*i+I, 1+(k+1)*j+J, ch) = A(1,1) + A(2,1)*I*hIk + A(3,1)*J*hIk + A(4,1)*I*J*(hIk^2);
                         endfor
                     endfor
 
@@ -112,14 +104,14 @@ function [] = decompress(compressed_image, method, k, h)
                     M(4, 3) = dxdy(i+2, j+1, ch);
                     M(3, 4) = dxdy(i+1, j+2, ch);
                     M(4, 4) = dxdy(i+2, j+2, ch);
-
-                    A = B_inv * M * (B_inv');
+                    
+                    A = (B \ M) / B'; %Resolve o sistema B*A*B' = M
                     % Iterando sobre os pixels do quadrado, inclui todas as arestas
-                    for di = 0:k+1
-                        for dj = 0:k+1
-                            v_x = [1, di, di*di, di*di*di];
-                            v_y = [1; dj; dj*dj; dj*dj*dj];
-                            decmp_img(1+(k+1)*i+di, 1+(k+1)*j+dj, ch) = v_x * A * v_y;
+                    for I = 0:k+1
+                        for J = 0:k+1
+                            v_x = [1, I*hIk, (I*hIk)^2, (I*hIk)^3];
+                            v_y = [1; J*hIk; (J*hIk)^2; (J*hIk)^3];
+                            decmp_img(1+(k+1)*i+I, 1+(k+1)*j+J, ch) = v_x * A * v_y;
                         endfor
                     endfor
 
